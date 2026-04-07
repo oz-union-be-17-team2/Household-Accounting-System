5f96b6c3dcd81d2404deee2bfb9b9b5b9408c268
netrunner <netrunnerr25@gmail.com>
2026-04-07 18:27:08 +0900

    ✨ feat     : user 모델 추가

--- household_budget/app/user/models.py
+++ household_budget/app/user/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from household_budget.core.models import TimeStampModel
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("올바른 이메일을 입력해주세요.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(TimeStampModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_delete = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = '유저'
+        verbose_name_plural = f'{verbose_name} 목록'
+
+    def __str__(self):
+        return self.email
