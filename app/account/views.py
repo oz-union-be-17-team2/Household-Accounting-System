fbb95a00518e4e55a552b7688f663c1277e900a0
yimiro <s_icay@naver.com>
2026-04-08 16:31:07 +0900

    precommit test

--- app/account/views.py
+++ app/account/views.py
@@ -1,10 +1,8 @@
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
-
 from app.account.models import Account
 from app.account.serializers import AccountListCreateSerializer
-
 from rest_framework.response import Response
 
 class AccountListCreateAPIView(APIView):
