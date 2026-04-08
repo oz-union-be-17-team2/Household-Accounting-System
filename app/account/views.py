c308ec2f5ca45b148211cc3026b85034dc605282
yimiro <s_icay@naver.com>
2026-04-08 16:19:53 +0900

    feat: setting base에 페이지네이션 옵션 추가, 스토리에 따른 모델 변경, list, Create 시리얼라이저, 뷰 추가

--- app/account/views.py
+++ app/account/views.py
@@ -1,37 +1,25 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
 
-from .models import Transaction
-from .serializers import TransactionSerializer
+from app.account.models import Account
+from app.account.serializers import AccountListCreateSerializer
 
+from rest_framework.response import Response
 
-class TransactionListView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = TransactionSerializer
+class AccountListCreateAPIView(APIView):
 
-    def get_queryset(self):
-        transaction = Transaction.objects.filter(user=self.request.user)
+    def get(self, request):
+        account_list= Account.objects.all().select_related('user')
+        paginator = PageNumberPagination()
+        queryset = paginator.paginate_queryset(account_list, request)
+        serializer = AccountListCreateSerializer(queryset, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
-        transaction_type = self.request.query_params.get("type")
-        amount_min = self.request.query_params.get("amount_min")
-        amount_max = self.request.query_params.get("amount_max")
+    def post(self, request):
+        serializer = AccountListCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
 
-        if transaction_type:
-            transaction = transaction.filter(transaction_type=transaction_type)
-        if amount_min:
-            transaction = transaction.filter(amount__gte=amount_min)
-        if amount_max:
-            transaction = transaction.filter(amount__lte=amount_max)
-
-        return transaction
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-
-class TransactionDetailView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = TransactionSerializer
-
-    def get_queryset(self):
-        return Transaction.objects.filter(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
